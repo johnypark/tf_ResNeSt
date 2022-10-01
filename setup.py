0528704da833fa8950571fcf75c2b8e3a207a38f
johnypark <parkjohnyc@gmail.com>
2022-10-01 12:56:39 -0400

    Update for pip install

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tf_ResNeSt_RegNet", # Replace with your own username
+    version="1.0",
+    author="QiaoranC",
+    author_email="QiaoranC",
+    description="TF-ResNest",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/QiaoranC/tf_ResNeSt_RegNet_model",
+    packages=setuptools.find_packages(),
+    install_requires = ['tensorflow'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
